5114d72731b54fcf8a9344a5006a212e0b5eebd1
David Barrera <davsanba@gmail.com>
2017-09-24 16:32:20 -0500

    Arreglado update y destroy

--- sam_api_gateway/app/controllers/users_controller.rb
+++ sam_api_gateway/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   skip_before_action :validate_token, only: [:create_user]
+  before_action :use_token, only: [:update_user, :destroy_user]
 
 # No dependen del token
     def create_user
@@ -15,7 +16,6 @@ class UsersController < ApplicationController
       end
     end
 
-
 # Dependen del token
 
     def index_user
@@ -29,19 +29,19 @@ class UsersController < ApplicationController
     end
 
     def update_user
-      user = HTTParty.put(ms_ip("rg")+"/users/update" + params[:id].to_s, body: {
-          firstName: params[:first_name], lastName: params[:last_name],
-          userName: params[:username], password: params[:password],
-          gender: params[:gender], dateBirth: params[:date_birth],
-          mobilePhone: params[:mobile_phone],
-          currentEmail: params[:current_email], location: params[:location]
-      }.to_json , headers:{
-        "Authorization": request.headers['AUTHORIZATION']})
+      value = user_params.to_h
+      value.deep_transform_keys!{ |key| key.to_s.camelize(:lower) }
+      value["userName"] = value.delete("username")
+      update_user = HTTParty.put(ms_ip("rg")+"/users/"+@username, body: value, query:{user:value})
+      if update_user.code == 200
+        render status: 200, json: {body:{message: "Usuario actualizado"}}.to_json
+      else
+        render status: update_user.code, json: update_user.body
+      end
     end
 
     def destroy_user
-      results = HTTParty.delete(ms_ip("rg")+"/users/destroy/" +params[:id].to_s,
-      headers:{ "Authorization": request.headers['AUTHORIZATION']})
+      results = HTTParty.delete(ms_ip("rg")+"/users/"+@username)
       if results.code == 200
         render status: 200, json: {body:{message: "Usuario borrado"}}.to_json
       else
@@ -49,8 +49,16 @@ class UsersController < ApplicationController
       end
     end
 
+    private
+
     def user_params
       params.require(:user).permit(:first_name, :last_name, :username, :password, :gender, :date_birth, :mobile_phone, :current_email, :location)
     end
 
+
+    def use_token
+      if params[:username] != @username
+        render status:400, json:{message: "No autorizado"}
+      end
+    end
 end
