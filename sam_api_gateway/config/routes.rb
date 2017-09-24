2705c1bb37572725b97ddfbf7423ae74da7612ab
Unknown <ykchautai@unal.edu.co>
2017-09-24 10:10:35 -0500

    Routes users

--- sam_api_gateway/config/routes.rb
+++ sam_api_gateway/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'refreshtoken', to: 'sessions#refresh_token'
   post 'users/login', to: 'sessions#login'
   get 'users/index', to: 'users#index_user'
-  get 'users/show', to: 'users#show_user'
-  post 'users/update', to: 'users#update_user'
+  get 'users/show/:id', to: 'users#show_user'
+  post 'users/update/:id', to: 'users#update_user'
   post 'users/create', to: 'users#create_user'
-  delete 'users/destroy', to: 'users#destroy_user'
+  delete 'users/destroy/:id', to: 'users#destroy_user'
 
 end
