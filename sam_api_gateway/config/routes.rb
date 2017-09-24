963a4990a2ac25ae44c2215a2e2690fe1934d622
Jesus Alejandro Noguera <janoguerab@unal.edu.co>
2017-09-23 23:21:56 -0500

    Routes for send mail

--- sam_api_gateway/config/routes.rb
+++ sam_api_gateway/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'refreshtoken', to: 'sessions#refresh_token'
   post 'users/login', to: 'sessions#login'
 
+  #SendMail
+  get 'sentMails', to: 'mail#sentMails'
+  get 'sentMail', to: 'mail#sentMail'
+  post 'sendMail', to: 'mail#sendMail'
+  
 end
