98a011a6b73e99b17d7c288ca8ac498c03ef99f4
Unknown <ykchautai@unal.edu.co>
2017-09-24 00:58:04 -0500

    Commit users controller

--- sam_api_gateway/app/controllers/users_controller.rb
+++ sam_api_gateway/app/controllers/users_controller.rb
@@ -1,3 +1,42 @@
 class UsersController < ApplicationController
 
+  skip_before_action :validate_token, only: [:create_user, :index_user, :show_user]
+
+    def create_user
+      user = HTTParty.post(ms_ip("rg")+"/users/create", body: {
+          firstName: params[:first_name], lastName: params[:last_name],
+          userName: params[:username], password: params[:password],
+          gender: params[:gender], dateBirth: params[:date_birth],
+          mobilePhone: params[:mobile_phone],
+          currentEmail: params[:current_email], location: params[:location]
+      }.to_json)
+    end
+
+    def index_user
+      results = HTTParty.get(ms_ip("rg")+"/users")
+      return results
+    end
+
+    def show_user(id)
+      results = HTTParty.get(ms_ip("rg")+"/users/" + id.to_s)
+      return results
+    end
+
+    def update_user(id)
+      user = HTTParty.put(ms_ip("rg")+"/users/update" + id.to_s, body: {
+          firstName: params[:first_name], lastName: params[:last_name],
+          userName: params[:username], password: params[:password],
+          gender: params[:gender], dateBirth: params[:date_birth],
+          mobilePhone: params[:mobile_phone],
+          currentEmail: params[:current_email], location: params[:location]
+      }.to_json , headers:{
+        "Authorization": request.headers['AUTHORIZATION']})
+    end
+
+    def destroy_user
+      results = HTTParty.delete(ms_ip("rg")+"/users/destroy/" + id.to_s ,
+      headers:{ "Authorization": request.headers['AUTHORIZATION']})
+      return results
+    end
+
 end
--- sam_api_gateway/config/routes.rb
+++ sam_api_gateway/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   get 'refreshtoken', to: 'sessions#refresh_token'
   post 'users/login', to: 'sessions#login'
+  get 'users/index', to: 'users#index_user'
+  get 'users/show', to: 'users#show_user'
+  post 'users/update', to: 'users#update_user'
+  post 'users/create', to: 'users#create_user'
+  delete 'users/destroy', to: 'users#destroy_user'
 
 end
